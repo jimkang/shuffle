e84aed2fb0c44ffa37fb3668752e195ffd8128c2
Jim Kang <jimkang@gmail.com>
2025-01-27 22:04:28 -0500

    Render results.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import { URLStore } from '@jimkang/url-store';
 import handleError from 'handle-error-web';
 import { version } from './package.json';
 import { renderDeck } from './renderers/render-deck';
+import { renderResults } from './renderers/render-results';
+import { shuffle } from 'probable';
 
 var urlStore;
+var draws = [];
+var currentIndex = -1;
 
 (async function go() {
   window.onerror = reportTopLevelError;
@@ -21,6 +25,7 @@ var urlStore;
 
 function onUpdate({ cards }) {
   renderDeck({ cards, addCard, onCardChanged });
+  renderResults({ runShuffle, drawNext, draws });
 
   function addCard() {
     cards.push('Edit this');
@@ -30,6 +35,19 @@ function onUpdate({ cards }) {
   function onCardChanged({ newText, order }) {
     cards[order] = newText;
     urlStore.update({ cards });
+  }
+
+  function runShuffle() {
+    cards = shuffle(cards);
+  }
+
+  function drawNext() {
+    currentIndex += 1;
+    if (currentIndex > cards.length) {
+      currentIndex = 0;
+    }
+    draws.push(cards[currentIndex]);
+    renderResults({ runShuffle, drawNext, draws });
   }
 }
 
---
+++ renderers/render-results.js
@@ -0,0 +1,14 @@
+import { select } from 'd3-selection';
+
+var shuffleButtonSel = select('#shuffle-deck-button');
+var drawButtonSel = select('#draw-button');
+var drawsListSel = select('#draws-list');
+
+export function renderResults({ runShuffle, drawNext, draws }) {
+  shuffleButtonSel.on('click', runShuffle);
+  drawButtonSel.on('click', drawNext);
+  var drawsSel = drawsListSel.selectAll('.draw').data(draws);
+  drawsSel.exit().remove();
+  var newDrawsSel = drawsSel.enter().append('li').classed('draw', true);
+  newDrawsSel.merge(drawsSel).text((x) => x);
+}
